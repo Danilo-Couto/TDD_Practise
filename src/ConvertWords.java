3087fe1c6f94db3fc2fa8b1349262a46dc2366ab
Danilo Couto <danilocoutodev@gmail.com>
2024-04-29 15:07:07 -0300

    refactor

--- src/ConvertWords.java
+++ src/ConvertWords.java
@@ -1,49 +1,58 @@
 public class ConvertWords {
 
-    // 6o caso: throw erros if StartWithNumber and EspecialChars
+    // refatoração completa
     public static String converterCamelCase(String original) {
-        if (Character.isDigit(original.charAt(0))) {
-            throw new StartWithNumberException("Inválido → não deve começar com números");
-        }
-
         char[] originalToArray = original.toCharArray();
 
-        for (char c: originalToArray) {
-            if (!Character.isAlphabetic(c) && !Character.isDigit(c)) {
-                throw new EspecialCharsException(" Inválido → caracteres especiais não são permitidos, somente letras e números");
-            }
-        }
+        checkFirstChar(original);
+        checkForSpecialCharacters(originalToArray);
 
         if(original.equals(original.toUpperCase())) {
             return original;
         } else {
             StringBuilder tempString = new StringBuilder();
-            char nextChar = originalToArray[1];
-            boolean firstOcurrence = true;
-
-            for (int i = 0; i < originalToArray.length; i++) {
-                if (i+1 < originalToArray.length) nextChar = originalToArray[i+1];
-
-                char actualChar = originalToArray[i];
-                if (Character.isUpperCase(actualChar) || Character.isDigit(actualChar)) {
-
-                    if((Character.isUpperCase(nextChar) || Character.isDigit(actualChar)) && firstOcurrence) {
-                        tempString.append(", ");
-                        tempString.append(originalToArray[i]);
-                        firstOcurrence = false;
-                    } else if (Character.isUpperCase(nextChar) && !firstOcurrence) {
-                        tempString.append(originalToArray[i]);
-                    } else {
-                        tempString.append(", ");
-                        tempString.append(Character.toLowerCase(originalToArray[i]));
-                    }
-
-                } else {
-                    tempString.append(originalToArray[i]);
-                }
-
-            }
+            convertOriginal(originalToArray, tempString);
             return tempString.toString();
+        }
+    }
+
+    private static void convertOriginal(char[] originalToArray, StringBuilder tempString) {
+        char nextChar = originalToArray[1];
+        boolean firstOcurrence = true;
+
+        for (int i = 0; i < originalToArray.length; i++) {
+            if (i+1 < originalToArray.length) nextChar = originalToArray[i+1];
+
+            char actualChar = originalToArray[i];
+            char lowerCaseChar = Character.toLowerCase(actualChar);
+
+            if (Character.isUpperCase(actualChar) || Character.isDigit(actualChar)) {
+                if((Character.isUpperCase(nextChar) || Character.isDigit(actualChar)) && firstOcurrence) {
+                    tempString.append(", ").append(originalToArray[i]);
+                    firstOcurrence = false;
+                } else if (Character.isUpperCase(nextChar) && !firstOcurrence) {
+                    tempString.append(originalToArray[i]);
+                } else {
+                    tempString.append(", ").append(lowerCaseChar);
+                }
+            } else {
+                tempString.append(originalToArray[i]);
+            }
+        }
+    }
+
+
+    private static void checkForSpecialCharacters(char[] originalToArray) {
+        for (char c: originalToArray) {
+            if (!Character.isLetterOrDigit(c)) {
+                throw new EspecialCharsException(" Inválido → caracteres especiais não são permitidos, somente letras e números");
+            }
+        }
+    }
+
+    private static void checkFirstChar(String original) {
+        if (Character.isDigit(original.charAt(0))) {
+            throw new StartWithNumberException("Inválido → não deve começar com números");
         }
     }
 
